e4c1a1e231d8bdfa5be32a44041840cd8f65d6ed
Daniel Tilly <44900781+crispyscripts@users.noreply.github.com>
2020-01-20 22:37:37 +0000

    Update collision.js

--- scripts/collision.js
+++ scripts/collision.js
@@ -100,8 +100,8 @@ for (var i = 0; i < totalBalls; i ++) {
     while (checkCount < 100 && findEmptyPlace(i, x, y, radius))
 
     // create params for a new ball
-    var dx = (Math.random() * 0.2);
-    var dy = (Math.random() * 0.2);
+    var dx = (Math.random() * -0.5);
+    var dy = (Math.random() * -0.5);
 
     var red = Math.random() * 255  + 5;
     var green = Math.random() * 255 + 5;
